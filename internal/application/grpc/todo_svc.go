a9f6505f9e78f52709c038e2217e05b45f8ecd92
Roman Vizitiu <md.vizitiuroman@gmail.com>
2023-05-21 22:53:53 -0400

    handler: http error handling

--- internal/application/grpc/todo_svc.go
+++ internal/application/grpc/todo_svc.go
@@ -2,12 +2,16 @@ package grpc
 
 import (
 	"context"
+	"errors"
 
 	"github.com/vizitiuRoman/go-grpc-boilerplate/internal/common/adapter/db"
 	"github.com/vizitiuRoman/go-grpc-boilerplate/internal/common/adapter/log"
 	"github.com/vizitiuRoman/go-grpc-boilerplate/internal/common/adapter/server/grpc"
+	"github.com/vizitiuRoman/go-grpc-boilerplate/internal/domain"
 	"github.com/vizitiuRoman/go-grpc-boilerplate/internal/domain/service"
 	"github.com/vizitiuRoman/go-grpc-boilerplate/pkg/gen/api/pb"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
 
@@ -71,29 +75,50 @@ func (s *todoSVC) CreateTodo(ctx context.Context, input *pb.CreateTodoInput) (*p
 
 func (s *todoSVC) UpdateTodo(ctx context.Context, input *pb.UpdateTodoInput) (*pb.UpdateTodoOutput, error) {
 	todo, err := s.todoService.Update(ctx, input)
-	if err != nil {
-		return nil, err
-	}
 
-	return &pb.UpdateTodoOutput{Todo: todo}, nil
+	switch {
+	case errors.Is(err, domain.ErrNotFound):
+		return nil, status.Error(codes.NotFound, err.Error())
+
+	case errors.Is(err, domain.ErrAlreadyExists):
+		return nil, status.Error(codes.AlreadyExists, err.Error())
+
+	case err != nil:
+		return nil, status.Error(codes.Internal, err.Error())
+
+	default:
+		return &pb.UpdateTodoOutput{Todo: todo}, nil
+	}
 }
 
 func (s *todoSVC) DeleteTodo(ctx context.Context, input *pb.DeleteTodoInput) (*emptypb.Empty, error) {
 	err := s.todoService.Delete(ctx, input.Id)
-	if err != nil {
-		return nil, err
-	}
 
-	return &emptypb.Empty{}, nil
+	switch {
+	case errors.Is(err, domain.ErrNotFound):
+		return nil, status.Error(codes.NotFound, err.Error())
+
+	case err != nil:
+		return nil, status.Error(codes.Internal, err.Error())
+
+	default:
+		return &emptypb.Empty{}, nil
+	}
 }
 
 func (s *todoSVC) GetTodo(ctx context.Context, input *pb.GetTodoInput) (*pb.GetTodoOutput, error) {
 	todo, err := s.todoService.Find(ctx, input.Id)
-	if err != nil {
-		return nil, err
-	}
 
-	return &pb.GetTodoOutput{Todo: todo}, nil
+	switch {
+	case errors.Is(err, domain.ErrNotFound):
+		return nil, status.Error(codes.NotFound, err.Error())
+
+	case err != nil:
+		return nil, status.Error(codes.Internal, err.Error())
+
+	default:
+		return &pb.GetTodoOutput{Todo: todo}, nil
+	}
 }
 
 func (s *todoSVC) GetTodos(ctx context.Context, input *pb.GetTodosInput) (*pb.GetTodosOutput, error) {
