bbe6e98eb113c0350c0fd2ebb88e74c6a6f113da
Roman Vizitiu <md.vizitiuroman@gmail.com>
2024-07-31 17:08:40 +0300

    wip

--- client/client.go
+++ client/client.go
@@ -1,36 +1,27 @@
 package main
 
-import (
-	"context"
-	"fmt"
-
-	log "github.com/vizitiuRoman/go-grpc-boilerplate/pkg/adapter/logger"
-	todov1 "github.com/vizitiuRoman/go-grpc-boilerplate/pkg/adapter/todo/v1"
-	pb "github.com/vizitiuRoman/go-grpc-boilerplate/pkg/gen/todo/v1"
-)
-
 func main() {
-	logger, err := log.NewLogger(nil)
-	if err != nil {
-		panic(err)
-	}
-
-	ctx := context.Background()
-
-	client, err := todov1.NewTodoSVCClient(
-		ctx,
-		logger.WithComponent(ctx, "todo-svc-client"),
-		&todov1.Config{
-			Host:     "localhost",
-			GrpcPort: 3774,
-		},
-		nil, nil,
-	)
-
-	todos, err := client.GetTodos(context.Background(), &pb.GetTodosInput{})
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println(todos.Todos[0])
+	//logger, err := log.NewLogger(nil)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//ctx := context.Background()
+	//
+	//client, err := todov1.NewTodoSVCClient(
+	//	ctx,
+	//	logger.WithComponent(ctx, "todo-svc-client"),
+	//	&todov1.Config{
+	//		Host:     "localhost",
+	//		GrpcPort: 3774,
+	//	},
+	//	nil, nil,
+	//)
+	//
+	//todos, err := client.GetTodos(context.Background(), &pb.GetTodosInput{})
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//fmt.Println(todos.Todos[0])
 }
--- internal/infra/repo/utils.go
+++ internal/infra/repo/utils.go
@@ -1,28 +1,8 @@
 package repo
 
-import (
-	"database/sql"
-	"errors"
-
-	"github.com/lib/pq"
-	"github.com/vizitiuRoman/go-grpc-boilerplate/internal/domain"
-)
-
-func wrapErrNoRows(err error) error {
-	if errors.Is(err, sql.ErrNoRows) {
-		return domain.ErrNotFound
-	}
-	return err
-}
-
-func isUniqueViolation(err error) bool {
-	pgErr, ok := err.(*pq.Error)
-	return ok && pgErr.Code == "23505"
-}
-
-func wrapUniqueViolation(err error) error {
-	if isUniqueViolation(err) {
-		return domain.ErrAlreadyExists
-	}
-	return err
-}
+//func wrapErrNoRows(err error) error {
+//	if errors.Is(err, sql.ErrNoRows) {
+//		return domain.ErrNotFound
+//	}
+//	return err
+//}
